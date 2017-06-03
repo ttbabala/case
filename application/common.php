7e8473cf6fd0fa437617f85c7f111b94e8dc70d0
ttbabala <hqiotzx@126.com>
2017-06-03 18:28:45 +0800

    3rd

--- application/common.php
+++ application/common.php
@@ -10,3 +10,10 @@
 // +----------------------------------------------------------------------
 
 // 应用公共文件
+function bcryptHash($rowPassword,$round =8){
+    if($round < 4 || $round > 31) $round = 8;
+    $salt = '$2a$'.str_pad($round,2,'0',STR_PAD_LEFT).'$';
+    $randomValue = openssl_random_pseudo_bytes(16);
+    $salt.=substr(strtr(base64_encode($randomValue),'+','.'),0,22);
+    return crypt($rowPassword,$salt);
+}
--- application/manage/controller/Adduser.php
+++ application/manage/controller/Adduser.php
@@ -1,11 +1,36 @@
 <?php
     namespace app\manage\controller;
+    vendor('predis.autoload');
     use think\Controller;
-    class AddUser extends Controller{
+    use think\Request;
+    use think\cache\driver\Redis;
+    
+    class Adduser extends Controller{
         public function index(){
             $pageTitle  = '添加用户';
             $this -> assign('title',$pageTitle);
             return $this -> fetch('index');
         }
-    }
-?>
+        public function addUserHandle(){
+            $Request = Request::instance();
+            $username = null;
+            $password = null;
+            $username = $Request -> post('username');
+            $password = $Request -> post('password');
+            if( isset($username) && isset($password) ){
+                $hashedPassword = bcryptHash($password);
+               // return '用户名为：'.$username.'；密码为：'.$hashedPassword.';长度为：'.strlen($hashedPassword);
+                $redis = new \Predis\Client();
+                //获取一个用户自增ID
+                $userID = $redis -> incr('users:count');
+                //存储用户信息
+                $setRes = $redis -> hmset("user:{$userID}",array(
+                   'username' => $username,
+                    'password' => $hashedPassword,
+                ));
+                if($setRes == 'OK'){
+                    return '用户添加成功！';
+                }
+            }
+        }
+   }
